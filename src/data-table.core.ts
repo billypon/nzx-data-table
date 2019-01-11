94260ae60eef0c31a77220afc078b4e6adf341d3
billypon <billypon@gmail.com>
2019-01-11 18:08:36 +0800

    update core

--- src/data-table.core.ts
+++ src/data-table.core.ts
@@ -1,5 +1,5 @@
 import { Observable, of } from 'rxjs';
-import { share } from 'rxjs/operators';
+import { shareReplay } from 'rxjs/operators';
 
 import { NzxDataTableFns } from './data-table.type';
 
@@ -29,7 +29,7 @@ export class NzxDataTableCore<T = any> {
   }
 
   loadItems(state?: any): Observable<T[]> {
-    const observer = new Observable<T[]>(observer => {
+    const observable = new Observable<T[]>(observer => {
       const oldItems = this.dataItems;
       this.dataItems = [];
       this.filteredItems = [];
@@ -54,9 +54,9 @@ export class NzxDataTableCore<T = any> {
       },
       err => observer.error(err),
       () => observer.complete());
-    }).pipe(share());
-    observer.subscribe();
-    return observer;
+    }).pipe(shareReplay(1));
+    observable.subscribe();
+    return observable;
   }
 
   filterItems(filter?: any, items: T[] = this.dataItems): T[] {
